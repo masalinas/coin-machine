b6399b294e0d24a2191124d635290af16d33c3c5
Miguel Salinas Gancedo <miguel@UNDERGROUND.(none)>
2017-09-04 23:41:21 +0200

    last update

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var RED = require('node-red');
 
 var app = module.exports = loopback();
 
@@ -29,5 +30,46 @@ boot(app, __dirname, function(err) {
 
         // start socket.io server and publish under loopback
         app.io = require('socket.io')(server);
+
+        // Create the settings object - see default settings.js file for other options
+        var settings = {
+            // By default, the Node-RED UI is available at http://localhost:1880/
+            // The following property can be used to specifiy a different root path.
+            // If set to false, this is disabled.
+            httpAdminRoot: "/red",
+            // Some nodes, such as HTTP In, can be used to listen for incoming http requests.
+            // By default, these are served relative to '/'. The following property
+            // can be used to specifiy a different root path. If set to false, this is
+            // disabled.
+            httpNodeRoot: "/red/api",
+            // By default, all user data is stored in the Node-RED install directory. To
+            // use a different location, the following property can be used
+            userDir: ".nodered/",
+            // The file containing the flows. If not set, it defaults to flows_<hostname>.json
+            flowFile: 'flows.json',
+            // Anything in this hash is globally available to all functions.
+            // It is accessed as context.global.
+            // eg:
+            //    functionGlobalContext: { os:require('os') }
+            // can be accessed in a function block as:
+            //    context.global.os
+            functionGlobalContext: {
+                fs: require('fs'),
+                moment: require('moment'),
+                loopback: app
+            }
+        };
+
+        // Initialise the runtime with a server and settings
+        RED.init(server, settings);
+
+        // Serve the editor UI from /red
+        app.use(settings.httpAdminRoot, RED.httpAdmin);
+
+        // Serve the http nodes UI from /api
+        app.use(settings.httpNodeRoot, RED.httpNode);
+
+        // Start the node-red runtime
+        RED.start();
     }
 });
