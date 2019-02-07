cddd47a60cffb17b9d9805699b2d85cd82ea82cb
Miguel Salinas Gancedo <miguel@thingtrack.com>
2019-02-07 20:56:50 +0100

    last update

--- client/app/js/index.js
+++ client/app/js/index.js
@@ -115,286 +115,187 @@ angular.module('coinmachine', ['ui.router', 'kendo.directives', 'lbServices', 'n
             //$scope.comboboxMarketInterval.readonly(true);
         });
 
-        // initialize markers collection
-        $scope.markets = [];
-
         // initialize filter values
         $scope.startTimestamp = new Date();
         $scope.quantity = 1;
 
-        // get all tickers
-        const MAX_MARKETS = 3000; // default MAX MARKETS to recover all market symbols
-        Coinmarketcap.getTicker({limit: MAX_MARKETS})
-            .$promise
-            .then(function(tickers, responseHeaders) {
-                    if (tickers.length > 0)
-                        $scope.tickers = tickers;
-                    else
-                        $scope.tickers = [];
+        // initialize collections
+        $scope.marketCaps =[];
+        $scope.tickers = [];
 
-                    $scope.marketOptions = {
-                        dataSource: $scope.tickers,
-                        dataTextField: "name",
-                        dataValueField: "symbol",
-                        headerTemplate: '<div class="dropdown-header k-widget k-header">' +
-                        '<span>Logo</span>' +
-                        '<span>Market [Symbol]</span>' +
-                        '</div>',
-                        valueTemplate: "<div ng-class=\"'s-s-{{dataItem.symbol}}'\" style= \"float: left; margin-top: 3px; margin-right: 5px;\"></div>{{dataItem.name}} [{{dataItem.symbol}}]</span>",
-                        template: "<div ng-class=\"'s-s-{{dataItem.symbol}}'\" style= \"float: left; margin-top: 3px; margin-right: 5px;\"></div>{{dataItem.name}} [{{dataItem.symbol}}]</span>",
-                        footerTemplate: 'Total #: instance.dataSource.total() # coins found',
-                    };
-
-                    // refresh anyChart datasource
-                    $log.info($scope.tickers.length + ' tickers recovered');
+        var initializeTable = function initializeTable() {
+            // configure market tickers table
+            $scope.optionsCcxtGrid = {
+                toolbar: ["excel", "pdf"],
+                excel: {
+                    fileName: "Markets Export.xlsx",
+                    allPages: true
                 },
-                function(httpResponse) {
-                    var error = httpResponse.data.error;
-                    $log.error('Error querying tickers - ' + error.status + ": " + error.message);
-                });
-
-        // configure kendo-ui table for bittrex source
-        $scope.optionsGrid = {
-            toolbar: ["excel", "pdf"],
-            excel: {
-                fileName: "Markets Export.xlsx",
-                allPages: true
-            },
-            pdf: {
-                fileName: "Markets Export.pdf",
-                allPages: true
-            },
-            dataSource: {
-                data: $scope.markets,
-                schema: {
-                    model: {
-                        fields: {
-                            MarketName: { type: "string" },
-                            High: { type: "number" },
-                            Low: { type: "number" },
-                            Volume: { type: "number" },
-                            Last: { type: "number" },
-                            BaseVolume: { type: "number" },
-                            TimeStamp: { type: "date" },
-                            Bid: { type: "number" },
-                            Ask: { type: "number" },
-                            OpenBuyOrders: { type: "number" },
-                            OpenSellOrders: { type: "number" },
-                            PrevDay: { type: "number" },
-                            Created: { type: "date" }
+                pdf: {
+                    fileName: "Markets Export.pdf",
+                    allPages: true
+                },
+                dataSource: {
+                    data: $scope.tickers,
+                    schema: {
+                        model: {
+                            fields: {
+                                symbol: { type: "string" },
+                                low: { type: "number" },
+                                high: { type: "number" },
+                                bid: { type: "number" },
+                                ask: { type: "number" },
+                                last: { type: "number" },
+                                quoteVolume: { type: "number" },
+                                baseVolume: { type: "number" },
+                                datetime: { type: "date" }
+                            }
                         }
-                    }
+                    },
+                    pageSize: 50
                 },
-                pageSize: 50
-            },
-            scrollable: true,
-            sortable: true,
-            filterable: true,
-            resizable: true,
-            selectable: true,
-            columnMenu: true,
-            pageable: {
-                input: true,
-                numeric: false
-            },
-            columns: [
-                { field: "MarketName", title: "Market Name"},
-                { field: "High", title: "High"},
-                { field: "Low", title: "Low"},
-                { field: "Volume", title: "Volume"},
-                { field: "Last", title: "Last"},
-                { field: "BaseVolume", title: "Base Volume"},
-                { field: "TimeStamp", title: "Date", template: '#= kendo.toString(kendo.parseDate(TimeStamp), "dd/MM/yyyy HH:mm:ss")#'},
-                { field: "Bid", title: "Bid"},
-                { field: "Ask", title: "Ask"},
-                { field: "OpenBuyOrders", title: "Open Buy Orders"},
-                { field: "OpenSellOrders", title: "Open Sell Orders"},
-                { field: "PrevDay", title: "Previous Day"},
-                { field: "Created", title: "Created", template: '#= kendo.toString(kendo.parseDate(Created), "dd/MM/yyyy HH:mm:ss")#'}
-            ]
+                scrollable: true,
+                sortable: true,
+                filterable: true,
+                resizable: true,
+                selectable: true,
+                columnMenu: true,
+                pageable: {
+                    input: true,
+                    numeric: false
+                },
+                columns: [
+                    { field: "symbol", title: "Symbol", template: function(dataRow) {
+                            var symbol = dataRow.symbol.split("/")[0];
+
+                            return '<div class="s-s-' + symbol + '" style= "float: left; margin-top: 3px; margin-right: 5px;"></div>' + ' <span>' + dataRow.symbol + '</span>';
+                        }
+                    },
+                    { field: "low", title: "Low"},
+                    { field: "high", title: "High"},
+                    { field: "bid", title: "Bid"},
+                    { field: "ask", title: "Ask"},
+                    { field: "last", title: "Last"},
+                    { field: "quoteVolume", title: "Volume"},
+                    { field: "baseVolume", title: "Base Volume"},
+                    { field: "datetime", title: "Date", template: '#= kendo.toString(kendo.parseDate(datetime), "dd/MM/yyyy HH:mm:ss")#'}
+                ]
+            };
+
+            $scope.toolTipOptions = {
+                filter: ".k-header",
+                position: "top",
+                content: function(e) {
+                    if ($scope.marketGrid.columns[e.target.context.cellIndex] !== undefined)
+                        var content = $scope.marketGrid.columns[e.target.context.cellIndex].title;
+
+                    return content;
+                }
+            };
         };
 
-        $scope.optionsCcxtGrid = {
-            toolbar: ["excel", "pdf"],
-            excel: {
-                fileName: "Markets Export.xlsx",
-                allPages: true
-            },
-            pdf: {
-                fileName: "Markets Export.pdf",
-                allPages: true
-            },
-            dataSource: {
-                data: $scope.markets,
-                schema: {
-                    model: {
-                        fields: {
-                            symbol: { type: "string" },
-                            low: { type: "number" },
-                            high: { type: "number" },
-                            bid: { type: "number" },
-                            ask: { type: "number" },
-                            last: { type: "number" },
-                            quoteVolume: { type: "number" },
-                            baseVolume: { type: "number" },
-                            datetime: { type: "date" }
-                        }
-                    }
-                },
-                pageSize: 50
-            },
-            scrollable: true,
-            sortable: true,
-            filterable: true,
-            resizable: true,
-            selectable: true,
-            columnMenu: true,
-            pageable: {
-                input: true,
-                numeric: false
-            },
-            columns: [
-                { field: "symbol", title: "Symbol", template: function(dataRow) {
-                        var symbol = dataRow.symbol.split("/")[0];
+        var initializeGraph = function initializeGraph() {
+            // configure anystock ohlcv graph
+            var table = anychart.data.table();
 
-                        return '<div class="s-s-' + symbol + '" style= "float: left; margin-top: 3px; margin-right: 5px;"></div>' + ' <span>' + dataRow.symbol + '</span>';
-                    }
-                },
-                { field: "low", title: "Low"},
-                { field: "high", title: "High"},
-                { field: "bid", title: "Bid"},
-                { field: "ask", title: "Ask"},
-                { field: "last", title: "Last"},
-                { field: "quoteVolume", title: "Volume"},
-                { field: "baseVolume", title: "Base Volume"},
-                { field: "datetime", title: "Date", template: '#= kendo.toString(kendo.parseDate(datetime), "dd/MM/yyyy HH:mm:ss")#'}
-            ]
+            // initialize ohlcv graph collection
+            table.addData([]);
+
+            // price mapping the data
+            var priceMapping = table.mapAs();
+
+            priceMapping.addField('open', 1, 'first');
+            priceMapping.addField('high', 2, 'max');
+            priceMapping.addField('low', 3, 'min');
+            priceMapping.addField('close', 4, 'last');
+
+            // volume mapping the data
+            var volumeMapping = table.mapAs();
+            volumeMapping.addField('value', 5, 'sum');
+
+            $scope.chartInstance = anychart.stock();
+
+            // configure the price series
+            //$scope.chartInstance.plot(0).ohlc(priceMapping).name('ACME Corp.'); // OHLC Graph
+            $scope.chartInstance.plot(0).candlestick(priceMapping).name('ACME Corp.'); // Candlestick Graph
+
+            // configure the volume series
+            $scope.chartInstance.plot(1).column(volumeMapping).name('Volume');
+
+            $scope.chartInstance.padding(10, 10, 10, 50);
+            $scope.chartInstance.plot(1).height('30%');
+
+            $scope.chartInstance.plot(1).yAxis().labels().format(function () {
+                var val = this['tickValue'];
+                var neg = val < 0;
+                val = Math.abs(val);
+                if (val / 1e15 >= 1) {
+                    return (val / 1e9).toFixed(0) + 'Q';
+                } else if (val / 1e12 >= 1) {
+                    return (val / 1e9).toFixed(0) + 'T';
+                } else if (val / 1e9 >= 1) {
+                    return (val / 1e9).toFixed(0) + 'B';
+                } else if (val / 1e6 >= 1) {
+                    return (val / 1e6).toFixed(0) + 'M';
+                } else if (val / 1e3 >= 1) {
+                    return (val / 1e3).toFixed(0) + 'K';
+                }
+                return neg ? '-' + val : val;
+            });
+
+            $scope.stock =  $scope.chartInstance;
         };
 
-        // configure kendo-ui header table tooltips
-        $scope.toolTipOptions = {
-            filter: ".k-header",
-            position: "top",
-            content: function(e) {
-                if ($scope.marketGrid.columns[e.target.context.cellIndex] !== undefined)
-                    var content = $scope.marketGrid.columns[e.target.context.cellIndex].title;
+        // get all market caps
+        var getMarketCaps = function getMarketCaps(maxMarkets) {
+            Coinmarketcap.getTicker({limit: MAX_MARKETS})
+                .$promise
+                .then(function(marketCaps, responseHeaders) {
+                        if (marketCaps.length > 0)
+                            $scope.marketCaps = marketCaps;
+                        else
+                            $scope.marketCaps = [];
 
-                return content;
-            }
+                        $scope.marketOptions = {
+                            dataSource: $scope.marketCaps,
+                            dataTextField: "name",
+                            dataValueField: "symbol",
+                            headerTemplate: '<div class="dropdown-header k-widget k-header">' +
+                            '<span>Logo</span>' +
+                            '<span>Market (Symbol)</span>' +
+                            '</div>',
+                            valueTemplate: "<div ng-class=\"'s-s-{{dataItem.symbol}}'\" style= \"float: left; margin-top: 3px; margin-right: 5px;\"></div>{{dataItem.name}} ({{dataItem.symbol}})</span>",
+                            template: "<div ng-class=\"'s-s-{{dataItem.symbol}}'\" style= \"float: left; margin-top: 3px; margin-right: 5px;\"></div>{{dataItem.name}} ({{dataItem.symbol}})</span>",
+                            footerTemplate: 'Total #: instance.dataSource.total() # coins found',
+                        };
+
+                        // refresh anyChart datasource
+                        $log.info($scope.marketCaps.length + ' Market Caps recovered');
+                    },
+                    function(httpResponse) {
+                        var error = httpResponse.data.error;
+                        $log.error('Error querying tickers - ' + error.status + ": " + error.message);
+                    });
+        };
+
+        const MAX_MARKETS = 3000;
+        getMarketCaps(MAX_MARKETS);
+
+        // intialize Market tickers table
+        initializeTable();
+
+        // intialize Market ohlcv stock grah
+        initializeGraph();
+
+        // load market tickers
+        $scope.onTickerClick = function (event) {
+
         };
 
         // load market candles
         $scope.onGraphClick = function (event) {
-            Bittrex.getCandles({market: 'BTC-LTC', tickInterval: 'fiveMin', startTimestamp: $scope.startTimestamp})
-                .$promise
-                .then(function(candles, responseHeaders) {
-                        if (candles.result.length > 0)
-                            $scope.candles = JSON.parse(angular.toJson(candles.result));
-                        else
-                            $scope.candles = [];
 
-                        // create data table on loaded data
-                        var dataTable = anychart.data.table();
-
-                        // refresh anyChart datasource
-                        $log.info($scope.candles.length + ' candles recovered');
-                    },
-                    function(httpResponse) {
-                        var error = httpResponse.data.error;
-                        $log.error('Error querying candles - ' + error.status + ": " + error.message);
-                    });
         };
-
-        // load market summary
-        $scope.onMarketClick = function (event) {
-            Bittrex.getMarketSummaries()
-                .$promise
-                .then(function(markets, responseHeaders) {
-                    if (markets.length > 0)
-                        $scope.markets = JSON.parse(angular.toJson(markets));
-                    else
-                        $scope.markets = [];
-
-                    // refresh grid datasource
-                    $scope.marketGrid.dataSource.data($scope.markets);
-
-                    $log.info($scope.markets.length + ' markets recovered');
-                },
-                function(httpResponse) {
-                    var error = httpResponse.data.error;
-                    $log.error('Error querying markets - ' + error.status + ": " + error.message);
-                });
-        };
-
-        // paint anystock graph
-        var table = anychart.data.table();
-
-        table.addData([
-            ['2015-12-24', 511.53, 514.98, 505.79, 506.40, 1200],
-            ['2015-12-25', 512.53, 514.88, 505.69, 507.34, 900],
-            ['2015-12-26', 511.83, 514.98, 505.59, 506.23, 800],
-            ['2015-12-27', 511.22, 515.30, 505.49, 506.47, 1500],
-            ['2015-12-28', 510.35, 515.72, 505.23, 505.80, 980],
-            ['2015-12-29', 510.53, 515.86, 505.38, 508.25, 700],
-            ['2015-12-30', 511.43, 515.98, 505.66, 507.45, 450],
-            ['2015-12-31', 511.50, 515.33, 505.99, 507.98, 1200],
-            ['2016-01-01', 511.32, 514.29, 505.99, 506.37, 1500],
-            ['2016-01-02', 511.70, 514.87, 506.18, 506.75, 780],
-            ['2016-01-03', 512.30, 514.78, 505.87, 508.67, 670],
-            ['2016-01-04', 512.50, 514.77, 505.83, 508.35, 700],
-            ['2016-01-05', 511.53, 516.18, 505.91, 509.42, 800],
-            ['2016-01-06', 511.13, 516.01, 506.00, 509.26, 400],
-            ['2016-01-07', 510.93, 516.07, 506.00, 510.99, 350],
-            ['2016-01-08', 510.88, 515.93, 505.22, 509.95, 800],
-            ['2016-01-09', 509.12, 515.97, 505.15, 510.12, 1300],
-            ['2016-01-10', 508.53, 516.13, 505.66, 510.42, 1200],
-            ['2016-01-11', 508.90, 516.24, 505.73, 510.40, 1000]
-        ]);
-
-        // price mapping the data
-        var priceMapping = table.mapAs();
-
-        priceMapping.addField('open', 1, 'first');
-        priceMapping.addField('high', 2, 'max');
-        priceMapping.addField('low', 3, 'min');
-        priceMapping.addField('close', 4, 'last');
-        //priceMapping.addField('value', 4, 'last');
-
-        // volume mapping the data
-        var volumeMapping = table.mapAs();
-        volumeMapping.addField('value', 5, 'sum');
-
-        $scope.chartInstance = anychart.stock();
-
-        // set the price series
-        //$scope.chartInstance.plot(0).ohlc(priceMapping).name('ACME Corp.');
-        $scope.chartInstance.plot(0).candlestick(priceMapping).name('ACME Corp.');
-
-        // set the volume series
-        $scope.chartInstance.plot(1).column(volumeMapping).name('Volume');
-
-        $scope.chartInstance.padding(10, 10, 10, 50);
-        $scope.chartInstance.plot(1).height('30%');
-
-        $scope.chartInstance.plot(1).yAxis().labels().format(function () {
-            var val = this['tickValue'];
-            var neg = val < 0;
-            val = Math.abs(val);
-            if (val / 1e15 >= 1) {
-                return (val / 1e9).toFixed(0) + 'Q';
-            } else if (val / 1e12 >= 1) {
-                return (val / 1e9).toFixed(0) + 'T';
-            } else if (val / 1e9 >= 1) {
-                return (val / 1e9).toFixed(0) + 'B';
-            } else if (val / 1e6 >= 1) {
-                return (val / 1e6).toFixed(0) + 'M';
-            } else if (val / 1e3 >= 1) {
-                return (val / 1e3).toFixed(0) + 'K';
-            }
-            return neg ? '-' + val : val;
-        });
-
-        $scope.stock =  $scope.chartInstance;
 
         // connect socket.io client to socket server
         Socket.connect();
